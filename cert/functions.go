f5c22eb30e03af0af7763aa0122a94bdbe1b8ddb
Luis E. Muñoz <github@lem.click>
2019-05-03 19:53:35 -0700

    Implemented IMAP STARTTLS. Custom greeting parser

--- cert/functions.go
+++ cert/functions.go
@@ -190,6 +190,94 @@ func doSMTPStartTLS(nc net.Conn, spec string, config *tls.Config) ([]*x509.Certi
 	return ret, nil
 }
 
+func doIMAPStartTLS(nc net.Conn, spec string, config *tls.Config) ([]*x509.Certificate, error) {
+
+	var tconn *textproto.Conn
+	var msg string
+
+	conn := textproto.NewConn(nc)
+
+	// Setup STARTTLS (passing conn to the TLS layer) — force SNI in case it matters
+
+	nc.SetDeadline(time.Now().Add(TSTARTTLS))
+	_, err := conn.Cmd("1 STARTTLS")
+	if err != nil {
+		return nil, err
+	}
+
+	msg, err = conn.Reader.ReadLine()
+	if err != nil {
+		return nil, err
+	}
+
+	if !strings.HasPrefix(msg, "1 OK") {
+		return nil, fmt.Errorf(msg)
+	}
+
+	// At this point, we're ready to pass the socket to the underlying TLS layer
+
+	nc.SetDeadline(time.Now().Add(TTLS))
+	tc := tls.Client(nc, config)
+	tconn = textproto.NewConn(tc)
+
+	// At this point, we have a TLS connection initialized so let's pull the certs
+	// out of it to return no our caller. We need to send some traffic to populate
+	// state, so let's send a NOOP at this point.
+
+	nc.SetDeadline(time.Now().Add(TNOOP))
+	if _, err = tconn.Cmd("2 NOOP"); err != nil {
+		return nil, err
+	}
+
+	if _, err = tconn.Reader.ReadLine(); err != nil {
+		return nil, err
+	}
+
+	cs := tc.ConnectionState()
+	ret := cs.PeerCertificates
+
+	nc.SetDeadline(time.Now().Add(TQUIT))
+	if _, err = tconn.Cmd("3 LOGOUT"); err != nil {
+		return nil, err
+	}
+
+	if _, err = tconn.Reader.ReadLine(); err != nil {
+		return nil, err
+	}
+
+	return ret, nil
+}
+
+func getGreeting(conn *textproto.Conn) (string, error) {
+	msg := ""
+
+	for {
+		buf, err := conn.Reader.ReadLine()
+		if err != nil {
+			return "", err
+		}
+
+		// Is it IMAP? Then return right away
+		if strings.HasPrefix(buf, "* OK") {
+			return buf, nil
+		}
+
+		// Short-circuit SMTP with rejection (rate-limiting, etc)
+		if !strings.HasPrefix(buf, "2") {
+			return buf, ErrUnsupportedSTARTTLS
+		}
+
+		// We know that the response starts with 2 and has a space after the result
+		// code, so this is either a single line greeting or the end of a multi-line
+		// greeting.
+		if len(buf) > 4 && buf[3] == ' ' {
+			msg = fmt.Sprintf("%s%s", msg, buf)
+			return msg, nil
+		}
+		msg = fmt.Sprintf("%s%s", msg, buf)
+	}
+}
+
 // GetValidSTARTTLSCert connects to a server, determines the underlying protocol
 // and if supported, forwards to the correct handler method. Otherwise returns
 // an appropriate error.
@@ -206,7 +294,7 @@ func GetValidSTARTTLSCert(spec string, config *tls.Config) ([]*x509.Certificate,
 
 	// Accept any 2xx greeting or bust
 	nc.SetDeadline(time.Now().Add(TGreeting))
-	_, msg, err = conn.Reader.ReadResponse(2)
+	msg, err = getGreeting(conn)
 	if err != nil {
 		return nil, err
 	}
@@ -226,11 +314,11 @@ func GetValidSTARTTLSCert(spec string, config *tls.Config) ([]*x509.Certificate,
 			return nil, ErrNoSTARTTLS
 		}
 
-		// TODO: Call IMAP handler
-		panic("IMAP handler not implemented")
+		return doIMAPStartTLS(nc, spec, config)
 	}
 
-	return nil, ErrUnsupportedSTARTTLS
+	// return nil, ErrUnsupportedSTARTTLS
+	return nil, fmt.Errorf("Unhandled response <%s>", msg)
 }
 
 // GetValidPostgresCert connects to a SMTP server and retrieves and validates
